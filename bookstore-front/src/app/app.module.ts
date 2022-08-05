1d43a2cff49084d674c7cd9b5dbdba838bc93195
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-08-05 19:59:01 -0300

    Adiciona component bookstore-app

--- bookstore-front/src/app/app.module.ts
+++ bookstore-front/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { NavComponent } from './components/shared/nav/nav.component';
+import { BookstoreAppComponent } from './components/bookstore-app/bookstore-app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    BookstoreAppComponent
   ],
   imports: [
     BrowserModule,
---
+++ bookstore-front/src/app/components/bookstore-app/bookstore-app.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BookstoreAppComponent } from './bookstore-app.component';
+
+describe('BookstoreAppComponent', () => {
+  let component: BookstoreAppComponent;
+  let fixture: ComponentFixture<BookstoreAppComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BookstoreAppComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookstoreAppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
